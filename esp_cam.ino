cf01c5666110d8dd1d7ed9ea798821870ad839cf
Victor <victorvelea007@yahoo.com>
2021-01-12 13:36:40 -0500

    first commit

--- esp_cam.ino
+++ esp_cam.ino
@@ -27,179 +27,147 @@ const char* password = "smoothbreeze500";
 //#define CAMERA_MODEL_WROVER_KIT
 
 #if defined(CAMERA_MODEL_WROVER_KIT)
-  #define PWDN_GPIO_NUM    -1
-  #define RESET_GPIO_NUM   -1
-  #define XCLK_GPIO_NUM    21
-  #define SIOD_GPIO_NUM    26
-  #define SIOC_GPIO_NUM    27
-  
-  #define Y9_GPIO_NUM      35
-  #define Y8_GPIO_NUM      34
-  #define Y7_GPIO_NUM      39
-  #define Y6_GPIO_NUM      36
-  #define Y5_GPIO_NUM      19
-  #define Y4_GPIO_NUM      18
-  #define Y3_GPIO_NUM       5
-  #define Y2_GPIO_NUM       4  
-  #define VSYNC_GPIO_NUM   25
-  #define HREF_GPIO_NUM    23
-  #define PCLK_GPIO_NUM    22
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
 
 #elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       32
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       32
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
 
 #elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       17
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       17
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
 
 #elif defined(CAMERA_MODEL_AI_THINKER)
-  #define PWDN_GPIO_NUM     32
-  #define RESET_GPIO_NUM    -1
-  #define XCLK_GPIO_NUM      0
-  #define SIOD_GPIO_NUM     26
-  #define SIOC_GPIO_NUM     27
-  
-  #define Y9_GPIO_NUM       35
-  #define Y8_GPIO_NUM       34
-  #define Y7_GPIO_NUM       39
-  #define Y6_GPIO_NUM       36
-  #define Y5_GPIO_NUM       21
-  #define Y4_GPIO_NUM       19
-  #define Y3_GPIO_NUM       18
-  #define Y2_GPIO_NUM        5
-  #define VSYNC_GPIO_NUM    25
-  #define HREF_GPIO_NUM     23
-  #define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 #else
-  #error "Camera model not selected"
+#error "Camera model not selected"
 #endif
 
-WiFiClient client;
-IPAddress addr(192,168,1,6);
+int port = 20000;
+String host = "71.63.36.251";
+
 
 void sendPhoto() {
   
   camera_fb_t * fb = esp_camera_fb_get();
-  if( !fb ) {
+  if ( !fb ) {
     Serial.println("Camera capture failed");
     delay(1000);
     ESP.restart();
-  } 
+  }
+  WiFiClient c;
+  if ( c.connect (host.c_str(), port) ) {
+    Serial.printf("Connected to %s!\n", host.c_str());
 
-
-  int len =fb->len;
-
-
-  Serial.println("Successfully captured picture");
-  if (WiFi.status() == WL_CONNECTED) {
-    String url = "http://192.168.1.8:20000/upload";
-    String host = "192.168.1.8";
-    WiFiClient c;
-
-    if ( c.connect(host.c_str(), 20000) ) {
-
-      Serial.println("Connection successful");
-
-      uint32_t buf_len = fb->len;
-      Serial.printf("Picture length: %u\n", buf_len);
-
-      String head = "";
-      String tail = "";
-
-      uint16_t extra_len = head.length() + tail.length();
-      uint32_t total_len = extra_len + buf_len;
-
-      // write the HTTP stuff
-      c.println("POST /upload HTTP/1.1");
-      c.println("Host: " + host + ":20000");
-      c.println("Content-Length: " + String(total_len));
-      c.println("Content-Type: image/jpeg");
-      c.println("Content-Disposition: 1.jpg");
-      c.println();
-      c.print(head);
-
-      // get a pointer to the frame buffer
-      uint8_t *fb_ptr = fb->buf;
-
-      // iterate through frame buffer and write the buffer in chunks
-      for (uint32_t i = 0; i < buf_len; i += 1024) {
-
-        if (i + 1024 < buf_len) {
-          c.write(fb_ptr, 1024);
-          fb_ptr += 1024;
-        
-        } else if (buf_len % 1024 > 0) {
-          c.write(fb_ptr, buf_len % 1024);
-
-          
-        }
-
-        
+    uint32_t buf_len = fb->len;
+    Serial.printf("Picture length: %u\n", buf_len);
+  
+  
+    // write the HTTP stuff
+    c.println("POST /upload HTTP/1.1");
+    c.println("Host: " + host + ":" + port);
+    c.println("Content-Length: " + String(buf_len));
+    c.println("Content-Type: image/jpeg");
+    c.println("Content-Disposition: 3.jpg");
+    c.println();
+  
+    int chunksize = 1024;
+    uint8_t * ptr = fb->buf;
+    for (int i = 0; i < buf_len; i += chunksize) {
+  
+      if ( i + chunksize < buf_len ) {
+        c.write(ptr, chunksize);
+        ptr += chunksize;
+      } else if ( buf_len % 1024 > 0 ) {
+        c.write(ptr, buf_len % 1024);
       }
-      c.print(tail);
-      
-    } else {
-      Serial.printf("Failed to connect to %s\n", host.c_str());
+  
     }
     
+  } else {
+
+    Serial.printf ("Failed to connect to %s...\n", host.c_str());
     
-
-  
-  } 
-
+  }
 
   esp_camera_fb_return(fb);
 
+ 
 
-
-  
 }
 
-void add_headers(HTTPClient *http, int len) {
-  http->addHeader("Content-Type", "image/jpeg");
-  http->addHeader("Content-Length", String(len));
-}
 
 
 
 
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
- 
+
   Serial.begin(115200);
   Serial.setDebugOutput(false);
-  
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -220,9 +188,9 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
@@ -231,7 +199,7 @@ void setup() {
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-  
+
   // Camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -246,12 +214,17 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  
+
   Serial.print("Camera Stream Ready! Go to: http://");
   Serial.print(WiFi.localIP());
   Serial.println();
-  
+
+
 }
+
+  
+
+
 
 void loop() {
   sendPhoto();
